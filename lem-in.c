a3e029d4031e531610e699074d0757e182fc9fbf
lamsnakesgit <cybvvow@gmail.com>
2019-12-17 18:02:33 +0300

    diffrntate rooms/comm-ands

--- lem-in.c
+++ lem-in.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddratini <ddratini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/17 15:42:13 by ddratini          #+#    #+#             */
+/*   Updated: 2019/12/17 18:00:15 by ddratini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lemin.h"
 /*
 ** amount of ants - ints?
@@ -16,7 +28,7 @@ unsigned int	amount_ants(int fd)
 
 	if (get_next_line(fd, &line) > 0)
 	{
-		if (ft_atoi(line) == 0)
+		if (ft_atoi(line) == 0)// < 1
 		{
 			free(line);
 			ft_err();
@@ -29,6 +41,14 @@ unsigned int	amount_ants(int fd)
 		return (0);
 	return (1);
 }
+/*char			**ft_split_room(char *line, char c)
+{
+	int i;
+}*/
+t_list			*ft_create_rooms(char *line)
+{
+	return (0);
+}
 /*
 ** if 1 line is ants_nums;
 ** * if not - or order or comment above ants -> display ERROR?
@@ -39,21 +59,66 @@ unsigned int	amount_ants(int fd)
 ** /submodeuls if u lno how to exec i will exec ,inflesliu
 ** actually delete putend; after- print_later
 ** also have to save fir-last rooms
+** rooms: own gnl cycle : if comm/st/end slip; if st/end -> next? saveline
+** else check_r-coords & save in lst
+** - room that has no link?
 */
-int				check_room(char *line)
-{//lineforma: "##start" "##end" "#comm" "nam1 cx cy"
+int				comstend(char *line)
+{
+	ft_putendl(line);//or after
 	if (line[0] == '#' && line[1] == '#' && line && line + 1)
 	{
-		if (!ft_strcmp(line + 2, "start") || !ft_strcmp(line + 2, "end"))
-			ft_putendl(line);//else ft_err(); its a comm#
+		if (!ft_strcmp(line + 2, "start"))// || !ft_strcmp(line + 2, "end")) unlnowm coms ignoreD
+		//else ft_err(); its a comm#
+		//	ft_create_rooms();
+			return (-1);
+		if (!ft_strcmp(line + 2, "end"))
+			return (-2);
 	}
-	else if (line[0] == '#')//mb shorter lines
-		ft_putendl(line);//return (1);
+	else if (line[0] == '#')
+		return (0);
 	else
-	{//usual nemeroom w coords two
-		if (ft_strchr(line, ' '))
+		return (1);
+}
+char				*room_save(char *line)
+{
+	char	**roomcor;
+	int 	i;
+	int		j;
+
+	i = 0;
+	roomcor = ft_strsplit(line, ' ');
+	while (roomcor[i] && roomcor)
+		++i;
+	if (i == 3)
+	{
+		j = -1;
+		while (roomcor[1][++j])
+			if (!ft_isdigit(roomcor[1][j]))
+				return (ft_err());
+		j = -1;
+		while (roomcor[2][++j])
+			if (!ft_isdigit(roomcor[2][j]))
+				return (ft_err());
 	}
-	return (0);
+}
+char				*check_room(char *line, int fd)
+{//lineforma: "##start" "##end" "#comm" "nam1 cx cy"
+	int		ret;
+//if not comm-s -> saveline-checked; somee var to kkeep countrun on rooms->then link follow
+	while (get_next_line(fd, line) > 0)
+	{
+		if ((ret = comstend(line)) == 0 || ret == -1 || ret == -2)
+			continue ;
+	/*	else if (ret == -1 || ret == -2)
+		{	save 1/end		
+		}*/
+		else
+		{//usual nemeroom w coords two//if (ft_strchr(line, ' '))//this maybe a room; save
+			room_save(line);
+		}
+	}
+	return (line);
 }
 
 int				val_in(int fd)
@@ -62,14 +127,9 @@ int				val_in(int fd)
 	char *line;
 
 	if (amount_ants(fd) > 0)
-	{//extra dop otd funs for chech_room w own w gnl
-		while (get_next_line(fd, &line) > 0)
-		{//first--check_roomspresence_validif
-		//	if (ft_strchr(line, ' '))
-				check_room(line);
-			//if (line && line[0] == '#')
-			//	;
-		}
+	{//extra dop otd for chech_room w own w gnl
+		//first--check_roomspresence_validif
+			check_room(line, fd);
 	}
 	return (1);
 }
--- lemin.h
+++ lemin.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lemin.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddratini <ddratini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/17 15:42:18 by ddratini          #+#    #+#             */
+/*   Updated: 2019/12/17 17:08:02 by ddratini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LEMIN_H
 #define LEMIN_H
 
@@ -6,8 +18,7 @@
 #include "libft/libft.h"
 #include <fcntl.h>
 /*
-**
-- [ ] validation_map:
+**[ ] validation_map:
 - [ ] # comment_start //in mid?
 - [ ] Number of ants: only digits:in line //w out \n
 - [ ] Ant_farm: blockkj:the_rooms: //aka name/ /c_x/ /x_y
@@ -16,22 +27,25 @@
 - [ ]   ##end -> name+coords w lastroom
 - [ ]  block_2 the_lineks:
 - [ ]   format: Name1-name2 
-** save during reading? where?
+- 0 ants? 0 room?
+** save during : line after n_A/start/end; until links-block s -1 start -2 end
 */
 typedef struct s_rooms
 {
 	char	*name_r;
-	struct s_rooms *next;
+	int		x;
+	int		y;
+//	t_ 		links;
+/*	struct s_rooms *next;
 	{
-		/* data */
 	};
-	
-}  			     t_rooms;
+*/	
+}				t_rooms;
 //struct that contains ptr to list /arr w rooms & ptr to lins??
 typedef	struct s_lemin
 {
-    long    nant;
-    t_
+	long	nant;
+	t_
 }      			 t_lemin;
 
 #endif
